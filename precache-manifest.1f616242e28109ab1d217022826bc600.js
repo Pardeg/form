2ba878aec5e38b2832b656d2a8c471765acac4bc
Pardeg <pardeg@gmail.com>
2020-05-29 00:39:48 +0300

    Updates

---
+++ precache-manifest.1f616242e28109ab1d217022826bc600.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cf809cb01bbf4b1879f701efee50ed1",
+    "url": "/form/index.html"
+  },
+  {
+    "revision": "0434e97cc92500f50768",
+    "url": "/form/static/css/main.2a3de6c5.chunk.css"
+  },
+  {
+    "revision": "5df5186542fa91a9f36d",
+    "url": "/form/static/js/2.c19d1801.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/form/static/js/2.c19d1801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0434e97cc92500f50768",
+    "url": "/form/static/js/main.facade78.chunk.js"
+  },
+  {
+    "revision": "d8cc87827d0c8cec296c",
+    "url": "/form/static/js/runtime-main.df3d993a.js"
+  },
+  {
+    "revision": "fc4db7ee86a2b160e9520aee39d5338f",
+    "url": "/form/static/media/Checkbox.fc4db7ee.svg"
+  },
+  {
+    "revision": "74238712fd7d703c9635fe2b67d6b1e9",
+    "url": "/form/static/media/Form.74238712.svg"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/form/static/media/OpenSans.629a55a7.ttf"
+  }
+]);
